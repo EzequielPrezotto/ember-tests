fadc06f56467617dbbbd691754572414527329f6
ezequielprezotto <eprezotto@apple.com>
2022-09-28 16:43:38 -0300

    Add faker

--- tests/unit/models/scientist-test.js
+++ tests/unit/models/scientist-test.js
@@ -7,7 +7,7 @@ module('Unit | Model | scientist', function (hooks) {
   // Replace this with your real tests.
   test('it exists', function (assert) {
     let store = this.owner.lookup('service:store');
-    let model = store.createRecord('scientist', {});
+    let model = store.createRecord('scientists', {});
     assert.ok(model);
   });
 });
